270f97582b8b50dc116e09f76ae742869c411e61
fwitt88 <fwitt88@icloud.com>
2026-05-28 13:26:14 +0200

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,9 +6,9 @@ const blog = defineCollection({
     title: z.string(),
     description: z.string(),
     pubDate: z.coerce.date(),
-    author: z.enum(['Dany', 'Kolibri', 'Florian]),
+    author: z.enum(['Dany', 'Kolibri', 'Florian']),
     category: z.string().optional(),
-    readingTime: z.number().optional(), // minutos
+    readingTime: z.number().optional(),
   }),
 });
 
